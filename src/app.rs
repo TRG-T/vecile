f3f8a9f73824829b80721fa3ea3853afd449f4fc
TARGEToff <lockontargt@gmail.com>
2021-12-28 19:34:50 +0100

    cleaned some code

--- src/app.rs
+++ src/app.rs
@@ -5,20 +5,13 @@ use std::fs;
 #[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
 #[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
-    // Example stuff:
-    label: String,
     default_path: String, 
-    
-    // this how you opt-out of serialization of a member
-    //#[cfg_attr(feature = "persistence", serde(skip))]
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            default_path: "./".to_string(),
+            default_path: ".".to_string(),
         }
     }
 }
@@ -52,9 +45,8 @@ impl epi::App for TemplateApp {
 
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
-    fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
+    fn update(&mut self, ctx: &egui::CtxRef, _frame: &mut epi::Frame<'_>) {
         let Self {
-            label,
             default_path,
         } = self;
 
@@ -64,33 +56,16 @@ impl epi::App for TemplateApp {
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-            egui::menu::bar(ui, |ui| {
-                egui::menu::menu(ui, "File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        frame.quit();
-                    }
-                });
-            });
-        });
-
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
-
             ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to("eframe", "https://github.com/emilk/egui/tree/master/eframe");
-                });
-            });
+                // egui::menu::bar(ui, |ui| {
+                //     egui::menu::menu(ui, "File", |ui| {
+                //         if ui.button("Quit").clicked() {
+                //             frame.quit();
+                //         }
+                //     });
+                // });
+                ui.label(&default_path);
+            })
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {

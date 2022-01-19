633071d7f52d8e00cd792882add357dd35c59604
TARGEToff <lockontargt@gmail.com>
2022-01-19 10:10:27 +0100

    optimised the program from ~30% CPU usage to ~1% by changing the input handling

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,24 @@ use crate::app::App;
 mod ui;
 use crate::ui::draw;
 use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event as CEvent, KeyCode},
+    event::{read, EnableMouseCapture, Event, KeyCode},
     execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen},
 };
-use std::{
-    error::Error,
-    io::stdout,
-    sync::mpsc,
-    thread,
-    time::{Duration, Instant},
-};
+//use crossterm_input::{InputEvent, KeyEvent, MouseButton, MouseEvent};
+use std::{error::Error, io::stdout};
 use tui::{backend::CrosstermBackend, Terminal};
-
-enum Event<I> {
-    Input(I)
-}
 
 #[derive(Debug)]
 struct Cli {
-    /// time in ms between two ticks.
-    tick_rate: u64,
     /// whether unicode symbols are used to improve the overall look of the app
     enhanced_graphics: bool,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let cli = Cli { tick_rate: 250, enhanced_graphics: true };
+    let cli = Cli {
+        enhanced_graphics: true,
+    };
 
     enable_raw_mode()?;
 
@@ -38,42 +29,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    // Setup input handling
-    let (tx, rx) = mpsc::channel();
-
-    let tick_rate = Duration::from_millis(cli.tick_rate);
-    thread::spawn(move || {
-        let last_tick = Instant::now();
-        loop {
-            // poll for tick rate duration, if no events, sent tick event.
-            let timeout = tick_rate
-                .checked_sub(last_tick.elapsed())
-                .unwrap_or_else(|| Duration::from_secs(0));
-            if event::poll(timeout).unwrap() {
-                if let CEvent::Key(key) = event::read().unwrap() {
-                    tx.send(Event::Input(key)).unwrap();
-                }
-            }
-        }
-    });
-
     let mut app = App::new("Filemng", cli.enhanced_graphics);
 
     terminal.clear()?;
     loop {
         terminal.draw(|f| draw(f, &mut app))?;
-        match rx.recv()? {
-            Event::Input(event) => match event.code {
-                KeyCode::Char('q') => {
-                    disable_raw_mode()?;
-                    execute!(
-                        terminal.backend_mut(),
-                        LeaveAlternateScreen,
-                        DisableMouseCapture
-                    )?;
-                    terminal.show_cursor()?;
-                    break;
-                }
+        match read()? {
+            Event::Key(event) => match event.code {        
                 KeyCode::Char(c) => app.on_key(c),
                 KeyCode::Left => app.on_left(),
                 KeyCode::Up => app.on_up(),
@@ -82,12 +44,15 @@ fn main() -> Result<(), Box<dyn Error>> {
                 KeyCode::Enter => app.on_enter(),
                 KeyCode::Esc => app.on_esc(),
                 _ => {}
-            },
+            }
+            _ => {}
         }
+
         if app.should_quit {
             break;
         }
     }
 
+    disable_raw_mode()?;
     Ok(())
 }
